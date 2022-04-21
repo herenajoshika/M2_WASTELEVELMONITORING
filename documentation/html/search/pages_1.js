0f49e8cbe09e5f72450301329683757e17907ee2
herenajoshika <101052348+herenajoshika@users.noreply.github.com>
2022-04-21 12:51:59 +0530

    Delete documentation directory
    DONE

--- documentation/html/search/pages_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['waste_20level_20monitoring_20application_20by_20herena_20joshika_20a_20s_0',['WASTE LEVEL MONITORING Application by HERENA JOSHIKA A S',['../index.html',1,'']]]
-];
